34246e66987da8c2f229e2e148f1b66a7c33177b
jakeogh <github.com@v6y.net>
2022-05-15 03:10:10 -0700

    auto-commit

--- .autogenerate_readme.sh
+++ .autogenerate_readme.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+## Examples:
+
 # if stdout is a tty (aka terminal):
 #tty:
 mpp 'a'
 
 # if stdout is not a tty
 mpp 'a' | od -tx1 -v
+
+# where c4 01 61 is:
+#  c4: bin8 https://github.com/msgpack/msgpack/blob/master/spec.md#bin-format-family
+#  01: one byte long
+#  61: a
+
--- mpp/mpp.py
+++ mpp/mpp.py
@@ -12,13 +12,21 @@ import msgpack
 signal(SIGPIPE, SIG_DFL)
 
 
+# since this is py3.8+, sys.argv: list[str]
 def main():
     if len(sys.argv) < 2:
-        print("Usage: mpp PATH...", file=sys.stderr)
+        print("Usage: mpp [PATH]...", file=sys.stderr)
         sys.exit(1)
 
     tty = sys.stdout.isatty()
-    argvb = list(map(os.fsencode, sys.argv[1:]))
+    # https://docs.python.org/3/library/os.html#python-utf-8-mode
+    # https://vstinner.github.io/pep-383.html
+    # https://github.com/python/cpython/blob/3.10/Lib/os.py#L804
+    # def fsencode():
+    #  filename.encode(encoding, errors)
+    #  filename.encode('utf8', 'surrogateescape')
+    # argvb = list(map(os.fsencode, sys.argv[1:]))
+    argvb = map(os.fsencode, sys.argv[1:])
     found_arg = False
     for arg in argvb:
         if len(arg) == 0:
